20bcb20608a593e9fd5e20ff0e2930820b10929e
raiviskarklins <raiviskarklins@gmail.com>
2017-03-06 10:28:10 +0200

    Fiddled with Player.cs

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -4,8 +4,11 @@ using UnityEngine;
 
 public class Player : MonoBehaviour {
 
+	private int timer;
+
 	private float hp;
 	private float armor;
+	private float hunger;
 	private Inventory[] playerInventory;
 
 	public float HP {
@@ -18,17 +21,49 @@ public class Player : MonoBehaviour {
 		set { this.armor = value; }
 	}
 
+	public float Hunger {
+		get { return this.hunger; }
+		set { this.hunger = value; }
+	}
+
 	public Inventory[] PlayerInventory{
 		get { return this.playerInventory; }
 		set { this.playerInventory = value; }
 	}
 
 
+	public void GetDamage(float multiplier){
+		float coef = 1f;
+
+		if (Armor != 0)
+			coef = 1f / Armor;
+
+		HP -= 0.5f * (multiplier * coef);
+	}
+
+	public void GetHunger(float multiplier){
+		Hunger -= 0.5f * multiplier; 
+	}
+
+	public void Eat(float multiplier){
+		Hunger += 0.5f * multiplier;
+	}
+
+	public void RegenHP(){
+		if (Hunger >= 20f) {
+			if (timer == 30) {
+				HP += 1f;
+				timer = 0;
+			}
+			timer++;
+		}
+	}
+
 
 
 	// Use this for initialization
 	void Start () {
-		
+		timer = 0;
 	}
 	
 	// Update is called once per frame
